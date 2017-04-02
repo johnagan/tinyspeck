bf93b950afe27549c1d1663a2b5ccf7da29de3ee
John Agan <john@slack-corp.com>
2017-04-01 19:23:20 -0700

    added support for websocket proxy

--- index.js
+++ index.js
@@ -18,10 +18,16 @@ class TinySpeck extends EventEmitter {
   /**
    * Contructor
    *
-   * @param {object} defaults - The default config for the instance
+   * @param {Object} defaults - The default config for the instance
    */
   constructor(defaults) {
     super()
+
+    // websocket server
+    this.socket = null
+
+    // http server
+    this.server = null
 
     // message defaults
     this.defaults = defaults || {}
@@ -34,7 +40,7 @@ class TinySpeck extends EventEmitter {
   /**
    * Create an instance of the TinySpeck adapter
    *
-   * @param {object} defaults - The default config for the instance
+   * @param {Object} defaults - The default config for the instance
    * @return {TinySpeck} A new instance of the TinySpeck adapter
    */
   instance(defaults) {
@@ -46,8 +52,8 @@ class TinySpeck extends EventEmitter {
   /**
    * Send data to Slack's API
    *
-   * @param {string} endPoint - The method name or url (optional - defaults to chat.postMessage)
-   * @param {object} args - The JSON payload to send
+   * @param {String} endPoint - The method name or url (optional - defaults to chat.postMessage)
+   * @param {Object} args - The JSON payload to send
    * @return {Promise} A promise with the API result
    */
   send(...args) {
@@ -58,7 +64,7 @@ class TinySpeck extends EventEmitter {
     let endPoint = 'chat.postMessage'
 
     // if an endpoint was passed in, use it
-    if (typeof args[0] === 'string') endPoint = args.shift()
+    if (typeof args[0] === 'String') endPoint = args.shift()
 
     // call update if ts included and no endpoint
     else if (message.ts) endPoint = 'chat.update'
@@ -80,17 +86,17 @@ class TinySpeck extends EventEmitter {
   /**
    * Parse a Slack message
    *
-   * @param {object|string} message - The incoming Slack message
-   * @return {object} The parsed message
+   * @param {Object|String} message - The incoming Slack message
+   * @return {Object} The parsed message
    */
   parse(message) {
-    if (typeof message === 'string') {
-      try { message = JSON.parse(message) } // JSON string
+    if (typeof message === 'String') {
+      try { message = JSON.parse(message) } // JSON String
       catch (e) { message = qs.parse(message) } // QueryString
     }
 
-    // interactive message payloads are JSON strings
-    if (typeof message.payload === 'string')
+    // interactive message payloads are JSON Strings
+    if (typeof message.payload === 'String')
       message = JSON.parse(message.payload)
 
     return message
@@ -100,7 +106,7 @@ class TinySpeck extends EventEmitter {
   /**
    * Notify a Slack message event
    *
-   * @param {object|string} message - The incoming Slack message
+   * @param {Object|String} message - The incoming Slack message
    * @return {Message} The parsed message
    */
   notify(message) {
@@ -144,9 +150,28 @@ class TinySpeck extends EventEmitter {
 
 
   /**
+   * Emit an event to listeners
+   * 
+   * @param {String} name - The event name emitted
+   * @param {any} message - The event message to emit
+   */
+  emit(name, message) {
+    super.emit(name, message)
+
+    // broadcast to open websockets
+    if (this.socket) {
+      let data = JSON.stringify({ name, message })
+      this.socket.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) client.send(data)
+      })
+    }
+  }
+
+
+  /**
    * Start RTM
    *
-   * @param {object} options - Optional arguments to pass to the rtm.start method
+   * @param {Object} options - Optional arguments to pass to the rtm.start method
    * @return {WebSocket} A promise containing the WebSocket
    */
   rtm(options) {
@@ -155,7 +180,7 @@ class TinySpeck extends EventEmitter {
       ws.on('message', this.notify.bind(this))
       ws.on('close', () => this.ws = null)
       ws.on('open', () => this.ws = ws)
-      return Promise.resolve(ws)
+      return ws
     })
   }
 
@@ -163,8 +188,8 @@ class TinySpeck extends EventEmitter {
   /**
    * OAuth Authorization Url
    *
-   * @param {object} params - The OAuth querystring params
-   * @return {string} The authorization url
+   * @param {Object} params - The OAuth queryString params
+   * @return {String} The authorization url
    */
   authorizeUrl(params) {
     return "https://slack.com/oauth/authorize?" + qs.stringify(params)
@@ -174,47 +199,92 @@ class TinySpeck extends EventEmitter {
   /**
    * WebServer to listen for WebHooks
    *
-   * @param {int} port - The port number to listen on
-   * @param {string} token - Optionally prodide a token to verify
-   * @return {listener} The HTTP listener
+   * @param {Number} port - The port number to listen on
+   * @param {String} token - (Optional) the Slack verification token
+   * @param {Boolean} enableWebSockets - Optionally enable the WebSockets proxy
+   * @return {http.Server} The HTTP server
    */
-  listen(port, token) {
-    let router = (req, res) => {
-      let body = []
-      req.on('data', body.push)
-      req.on('end', () => {
-        // update the request
-        let data = Buffer.concat(body).toString()
-        req.body = this.parse(data)
-        req.url = url.parse(req.url)
-        req.params = qs.parse(req.url.query)
-
-        // new subscription challenge
-        if (req.body.challenge) return res.end(req.body.challenge)
-
-        // reject unverified requests
-        if (token && token !== req.body.token) {
-          res.statusCode = 401
-          return res.end()
-        }
-
-        // notify route handler if available
-        if (this.eventNames().indexOf(req.url.pathname) !== -1) {
-          res.json = data => { // json helper
-            res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify(data))
-          }
-          this.emit(req.url.pathname, req, res)
-        } else {
-          // notify listeners of the event
-          this.notify(req.body)
-          res.end()
-        }
-      })
-    }
-
-    return http.createServer(router).listen(port, () => {
+  listen(port, token, enableWebSockets) {
+    // http server
+    let router = this.router.bind(this, token)
+    let server = http.createServer(router).listen(port, () => {
       console.log(`listening for events on http://localhost:${port}`)
+      if (enableWebSockets) this.proxy(server, token)
+    })
+
+    return server
+  }
+
+
+  /**
+   * Turn on the WebSocket proxy
+   * 
+   * @param {http.Server} server - The http server to attach to
+   * @param {String} token - The token to verify websocket connections with
+   * @param {String} paramName - (Optional) The param name to read the token from
+   */
+  proxy(server, token, paramName) {
+    paramName = paramName || 'token'
+    this.socket = new WebSocket.Server({ server })
+
+    // validate websocket connections
+    this.socket.on('connection', socket => {
+      let location = url.parse(socket.upgradeReq.url, true)
+      let callback = data => socket.send(JSON.stringify(data))
+
+      // redirect incoming messages to Slack
+      let onMessage = message => {
+        let data = JSON.parse(message)
+        if (data.method) this.send(data.method, data).then(callback)
+        else this.send(data).then(callback)
+      }
+
+      if (location.query[paramName] !== token) socket.close() // validate against token
+      else socket.on('message', onMessage)
+    })
+  }
+
+
+  /**
+   * HTTP Request Router
+   * 
+   * @param {String} token - The Slack verification token
+   * @param {Object} req - The HTTP request Object
+   * @param {Object} res - The HTTP response Object
+   */
+  router(token, req, res) {
+    let body = []
+    req.on('data', body.push.bind(body))
+    req.on('end', () => {
+      // update the request
+      let data = Buffer.concat(body).toString()
+      req.body = this.parse(data)
+      req.url = url.parse(req.url)
+      req.params = qs.parse(req.url.query)
+
+      // new subscription challenge
+      if (req.body.challenge) return res.end(req.body.challenge)
+
+      // reject unverified requests
+      if (token && token !== req.body.token) {
+        res.statusCode = 401
+        return res.end()
+      }
+
+      // json helper
+      res.json = data => {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(data))
+      }
+
+      // notify route handler if available
+      if (this.eventNames().indexOf(req.url.pathname) !== -1) {
+        this.emit(req.url.pathname, req, res)
+      } else {
+        // notify listeners of the event
+        this.notify(req.body)
+        res.end()
+      }
     })
   }
 
