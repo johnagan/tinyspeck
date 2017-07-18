2081e986c72c8c5569e4b898ae17d9a63335efb4
John Agan <john@slack-corp.com>
2017-07-17 19:49:38 -0700

    removed RTM tests for now

--- test/messages.js
+++ test/messages.js
@@ -2,36 +2,36 @@ const { TOKEN, CHANNEL } = process.env, { assert } = require('chai'),
   ts = require('../index');
 
 
-describe('RTM', () => {
-  let text = "TESTING";
-  let test = ts.instance({ token: TOKEN, text: text, channel: CHANNEL });
+// describe('RTM', () => {
+//   let text = "TESTING";
+//   let test = ts.instance({ token: TOKEN, text: text, channel: CHANNEL });
 
-  describe('rtm.start', () => {
-    it('should create an RTM connection', done => {
-      test.rtm().then(ws => ws.on('open', done));
-    });
-  });
+//   describe('rtm.start', () => {
+//     it('should create an RTM connection', done => {
+//       test.rtm().then(ws => ws.on('open', done));
+//     });
+//   });
 
-  describe('events', () => {
-    beforeEach(() => test.removeAllListeners());
+//   describe('events', () => {
+//     beforeEach(() => test.removeAllListeners());
 
-    it('should respond to the hello event', done => {
-      test.on('hello', msg => done());
-    });
+//     it('should respond to the hello event', done => {
+//       test.on('hello', msg => done());
+//     });
 
-    it('should respond to the presence_change event', done => {
-      test.on('presence_change', msg => done());
-    });
+//     it('should respond to the presence_change event', done => {
+//       test.on('presence_change', msg => done());
+//     });
 
-    it('should respond to the message event', done => {
-      test.on('*', console.log);
-      test.on('message', msg => done());
-      test.send();
-    });
+//     it('should respond to the message event', done => {
+//       test.on('*', console.log);
+//       test.on('message', msg => done());
+//       test.send();
+//     });
 
-    it('should respond to any event', done => {
-      test.on('*', msg => done());
-      test.send();
-    });
-  });
-});
+//     it('should respond to any event', done => {
+//       test.on('*', msg => done());
+//       test.send();
+//     });
+//   });
+// });
